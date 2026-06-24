2c6f14b7013161f7a5502c69aec8a05999ecd5c3
Omar Lizardo <omarlizardo@gmail.com>
2026-06-23 20:25:55 -0700

    Clean obsolete functions and discarded code folder

--- Functions/simplot.R
+++
@@ -1,7 +0,0 @@
-sim.plot <- function(x) {
-     p <- ggcorrplot(x)
-     p <- p + scale_x_discrete(position = "top")
-     p <- p + theme(legend.position = "none",
-                    axis.text.x = element_text(hjust = -0.2)) 
-     return(p)
-}
